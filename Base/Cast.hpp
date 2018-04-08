8ce40b057b15345597578ce8f1c6d5495243845b
huangxiaolong <hades717@live.cn>
2018-04-08 11:40:29 +0800

    MOD:修改cast转换demo； ADD:添加异常和信息回调demo

--- Base/Cast.hpp
+++ Base/Cast.hpp
@@ -16,6 +16,20 @@ public:
 	}
 };
 
+class VirtualBase {
+public:
+	virtual void f() {
+		std::cout << "VirtualBase::f()" << std::endl;
+	}
+};
+class VirtualDerived : public VirtualBase
+{
+public:
+	virtual void f() {
+		std::cout << "VirtualDerived::f()" << std::endl;
+	}
+};
+
 class Extras
 {
 public:
@@ -26,9 +40,25 @@ public:
 
 void TestDemo()
 {
-	std::shared_ptr<Base> base_ptr = std::make_shared<Base>();
-	std::shared_ptr<Derived> dervied_ptr = std::make_shared<Derived>();
-	std::shared_ptr<Extras> extra_ptr = std::make_shared<Extras>();
-	base_ptr = std::dynamic_pointer_cast<Base>(dervied_ptr);
-	base_ptr->f();
+	double d = 9.02;
+	double* pd = &d;
+	int* pi_re = reinterpret_cast<int*>(pd);
+	//int i_re = reinterpret_cast<int>(d);//不能转换非指针对象
+	std::cout << *pi_re << std::endl;
+	int i_static = static_cast<int>(d);	//基本类型的强转
+	std::cout << i_static << std::endl;
+
+	VirtualDerived dervied;
+	VirtualBase& base = static_cast<VirtualBase&>(dervied);//继承关系的转换
+	base.f();
+
+// 	Derived* dervied_ptr = new Derived;
+// 	Base* base_ptr = dynamic_cast<Base*>(dervied_ptr);	//非多态只能往下转换
+// 	base_ptr->f();
+
+// 	VirtualBase* base_ptr = new VirtualBase;
+// 	//多态可以往上转换，但是可能无效
+// 	VirtualDerived* dervied_ptr = dynamic_cast<VirtualDerived*>(base_ptr);	
+// 	if (dervied_ptr)
+// 		dervied_ptr->f();
 }
